a1f30c52a431d746d0c35576e580c13b192896a8
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-05-03 15:35:46 -0300

    A9 ajax na app

--- garconapp/www/js/app.js
+++ garconapp/www/js/app.js
@@ -56,3 +56,22 @@ $('.scan-qrcode').on('click', function() {
         }
     );
 });
+
+$('.acao-finalizar').click(function(){
+    $.ajax({
+        url: 'http://cozinhapp.sergiolopes.org/novo-pedido',
+        data: {
+            mesa: $('#numero-mesa').val(),
+            pedido: $('#resumo').text()
+        },
+        success: function(resposta) {
+            Materialize.toast(resposta, 2000);
+
+            $('#numero-mesa').val('');
+            $('.badge').remove();
+        },
+        error: function(erro) {
+            Materialize.toast(erro.responseText, 3000, `red-text`);
+        }
+    });
+});
