5dea3f0f6a05cb26fa5d6ec1074bc9c6459a6ae7
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-05-03 10:19:27 -0300

    A5 evoluindo a garconapp

---
+++ garconapp/www/js/app.js
@@ -0,0 +1,41 @@
+$('.collection-item').on('click', function() {
+
+    var $badge = $('.badge', this);
+
+    if ($badge.length == 0) {
+        $badge = $('<span class="badge brown-text">0</span>')
+        .appendTo(this);
+    }
+
+    $badge.text(parseInt($badge.text()) + 1);
+
+});
+
+$('#confirmar').on('click', function() {
+
+    var texto = '';
+
+    $('.badge').parent().each(function() {
+        var produto = this.firstChild.textContent;
+        var quantidade = this.lastChild.textContent;
+
+        texto += produto + ': ' + quantidade + ', ';
+    });
+
+    $('#resumo').text(texto);
+
+});
+
+$('.modal-trigger').leanModal();
+
+$('.collection').on('click', '.badge', function(){
+    $(this).remove();
+
+    return false;
+});
+
+$('.acao-limpar').on('click', function() {
+    $('#numero-mesa').val('');
+
+    $('.badge').remove();
+});
