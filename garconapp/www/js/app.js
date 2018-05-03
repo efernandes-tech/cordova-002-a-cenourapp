dfe108cf7fe54c42bbe3324817b68f2e32b62171
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-05-03 15:20:14 -0300

    A8 lendo codigo de barra na app

--- garconapp/www/js/app.js
+++ garconapp/www/js/app.js
@@ -42,3 +42,17 @@ $('.acao-limpar').on('click', function() {
 
     $('.badge').remove();
 });
+
+$('.scan-qrcode').on('click', function() {
+    cordova.plugins.barcodeScanner.scan(
+        function (resultado) {
+            if (resultado.text) {
+                Materialize.toast('Mesa ' + resultado.text, 2000);
+                $('#numero-mesa').val(resultado.text);
+            }
+        },
+        function (error) {
+            Materialize.toast('Erro: ' + error, 3000, 'red-text');
+        }
+    );
+});
